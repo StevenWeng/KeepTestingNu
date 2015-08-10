463b45e1f42a4d47114763e0ee5265efa8479a3d
StevenWeng <stevenweng@systex.com.tw>
2015-08-10 13:12:16 +0800

    init

---
+++ test/TestFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+
+[TestFixture]
+public class TestFixture1
+{
+	[Test]
+	public void TestTrue()
+	{
+		Assert.IsTrue(true);
+	}
+
+	// This test fail for example, replace result or delete this test to see all tests pass
+	[Test]
+	public void TestFault()
+	{
+		Assert.IsTrue(true);
+	}
+}
+
